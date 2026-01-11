b2e58ddd2cdd3d65f59444014dae982975c250a2
LDRMRQS <romulomarquesx@gmail.com>
2026-01-10 23:15:42 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
     <aside>
-        <img src="https://github.com/LDRMRQS.png" />
-        <Titulo fontSize={20}>Rômulo</Titulo>
+        <SidebarContainer>
+            <Avatar />
+            <Titulo fontSize={20}>Rômulo</Titulo>
+            <Paragrafo tipo="secundario" fontSize={16}>
+                LDRMRQS
+            </Paragrafo>
+            <Descricao tipo="principal" fontSize={12}>
+                Garotinho de programs
+            </Descricao>
+            <BotaoTema>Trocar tema</BotaoTema>
+        </SidebarContainer>
     </aside>
 )
 
